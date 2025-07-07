b7948024df3b12c9053d2bef970ea7cd76098c00
Sid-LD <siddhantroy2006@gmail.com>
2025-07-07 10:02:49 +0530

    done till all movies with optimised search

--- src/App.jsx
+++ src/App.jsx
@@ -40,21 +40,76 @@
 
 
 import React, { useEffect } from 'react'
-import Search from './components/Search'
+import Search from './components/Search.jsx'
 import { useState } from 'react'
-import { useEffect } from 'react'
+import Spinner from './components/Spinner.jsx'
+import MovieCard from './components/MovieCard.jsx'
+import { useDebounce } from 'react-use'
 
 const API_BASE_URL="https://api.themoviedb.org/3"
 
 const API_KEY=import.meta.env.VITE_TMDB_API_KEY
 
+const API_OPTIONS={
+  method:"GET",
+  headers:{
+    accept:"application/json",
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
+
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('') //you cannot change state variables
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+  //Debounce the search term to prevent making too many API request
+  //by waiting for the user to stop typing for 500ms
+  //We are debouncing the input field
+  useDebounce(()=> setDebouncedSearchTerm(searchTerm),500,[searchTerm])
+
+  const fetchMovies=async(query='')=>{
+    setErrorMessage('')
+    setIsLoading(true)
+    try {
+      const endpoint=query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc` // Builds the API URL for  discovering movies, sorted by popularity (most popular first)
+
+      const response=await fetch(endpoint,API_OPTIONS) //// Sends a GET request to that URL, using your API key for authorization
+      //returns a response object
+      if(!response.ok){
+        throw new Error("Failed to fetch movies")
+      }
+      
+
+      const data=await response.json()//parses the json string from respose object and returns a usable js object
+      if(data.Response=="False"){
+        setErrorMessage(data.Error || "Failed to fetch movies")
+        setMovieList([])
+        return
+      }
+      console.log(data);
+      
+      setMovieList(data.results)
+      
+      
+    } catch (error) {
+      console.error(`Error while fetching movies: ${error}`)
+      setErrorMessage("Error fetching movies, please try again")
+      
+    }
+    finally{
+      setIsLoading(false)
+    }
+  }
 
   useEffect(()=>{
-
-  })
+    fetchMovies(debouncedSearchTerm)
+  },[debouncedSearchTerm])
   return (
     <main>
 
@@ -64,10 +119,25 @@ const App = () => {
         <header>
           <img src="/hero.png" alt="Hero Banner" />
         <h1>Find <span className='text-gradient'>Movies</span> You will enjoy without the hassle</h1>
-        </header>
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-
+        </header>
+        <section className='all-movies'>
+          <h2 className='mt-6'>All movies</h2>
+          {isLoading?(
+            <p className='text-zinc-300'><Spinner/></p>
+          ) : errorMessage?(
+            <p className='text-red-600'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie)=>(
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )
+          
+          }
+        </section>
       </div>
     </main>
   )
